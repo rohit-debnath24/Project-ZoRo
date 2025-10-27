d1cdea1185f6c153610a921841a98dc572828ff4
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-10-27 19:54:40 +0530

    ft: product AddToCart

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { toast} from 'react-toastify';
 
 export const Login = () => {
-  const [currentState,setCurrentState]=useState('Sign Up');
+  const [currentState,setCurrentState]=useState('Login');
   const {token,setToken,navigate,backend_url}=useContext(ShopContext)
   const[name,setName]=useState('')
   const[email,setEmail]=useState('')
